3c5cd506130ec11fc318ab509e2df6700fea5432
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-11-24 19:02:39 -0500

    added greeting for new users and ability to remove users

--- index.js
+++ index.js
@@ -86,7 +86,26 @@ client.on('message', (message) => {
 
 // includes features like greeting new users, kicking, banning, permission handling, etc.
 
+// handles greeting of new users and removal of users
 
+const newUsers = [];
+
+client.on('guildMemberAdd', (member) => {
+  const guild = member.guild;
+  if(!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
+
+  if(newUsers[guild.id].size > 10) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(' ');
+    guild.channels.find(channel => channel.name === 'general').send(`Welcome, ${userlist}!`);
+    newUsers[guild.id].clear();
+  }
+});
+
+client.on('guildMemberRemove', (member) => {
+  const guild = member.guild;
+  if(newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
+});
 
 /////////////////////////////// ***** MUSIC BOT ***** ///////////////////////////////
 
