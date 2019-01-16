28e0e947fb0d920dd08a20592fffe50ca2b71c82
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-01-15 22:53:01 -0500

    removed some placeholders

--- index.js
+++ index.js
@@ -223,8 +223,9 @@ const playerID = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-nam
 const championID = "https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&dataById=false&api_key=" + lol_api;
 const getChampion = "http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json";
 const routeInfo = 'http://api.champion.gg/v2/champions?limit=200&champData=hashes,firstitems,summoners,skills,finalitemshashfixed,masterieshash&api_key=' + champggToken;
+
 ///// League API functions
-// placeholder 
+
 function getPlayerID(player, cb) {
   request(playerID + urlencode(summoner) + "?api_key=" + lol_api, function(error, response,body) {
     if(err) {
