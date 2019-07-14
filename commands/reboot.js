8d709678effbff69ecac7b582be36f59c90a5b2e
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-07-13 22:30:31 -0400

    created simple command handling in reboot.js

--- commands/reboot.js
+++ commands/reboot.js
@@ -1,9 +1,19 @@
 // NOTE: this file/command is again not for music playback but rather reboots the bot
 
 exports.run = async (client, message) => {
-    message.channel.sendMessage('Are you sure you want to reboot? Enter yes/y for yes, no/n for no, \`cancel\` to abort. Auto-abort in 30 seconds.');
+    message.channel.sendMessage('Are you sure you want to reboot? Enter yes/y for yes, no/n or \`cancel\` to abort. Auto-abort in 30 seconds.');
 
     const valid = ['yes', 'y', 'no', 'n', 'cancel'];
     const collector = message.channel.createCollector(m => m.author.id === message.author.id, { time: 30000 });
-    
+
+    collector.on('message', async m => {
+        const lower = m.content.toLowerCase();
+        if(lower === 'cancel' || lower === 'no' || lower === 'n') {
+            return collector.stop('abort');
+        } else if(lower === 'yes' || lower === 'y') {
+            return collector.stop('kill');
+        }
+
+        return message.channel.sendMessage(`Only \`${valid.join('`, `')}\` are valid, please use these!`).catch(console.error);
+    });
 }
