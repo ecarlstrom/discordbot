bf2f12a577a0f71d9d4103c80d12195d05622b7c
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-07-15 21:59:01 -0400

    completed first pass of git update code, may have to make some adjustments and/or updated credentials

--- commands/gitUpdate.js
+++ commands/gitUpdate.js
@@ -1,1 +1,32 @@
-// working on creating a file that will reboot the bot when the Git repo is updated
+const {
+    exec
+} = require('child_process');
+const path = require('path');
+const {
+    URL
+} = require('url');
+
+const {
+    run: reboot
+} = require('reboot');
+const config = require('../config.json');
+
+exports.run = async(client, message, args) => {
+    const gitURL = new URL(config.gitURL);
+    gitURL.password = config.gitPassword;
+    gitURL.username = config.gitUsername;
+
+    exec(`git pull ${gitURL}`, {
+        cwd: path.join(__dirname, '../')
+    }, async(err, stdout, stderr) => {
+        if(err) return console.error(err);
+
+        const out = [];
+        if(stdout) out.push(stdout);
+        if(stderr) out.push(stderr);
+
+        await message.channel.sendMessage(`\`\`\`${out.join('```\n```')}\`\`\``);
+
+        return reboot(client, message, args);
+    });
+};
