4c12e028be0f9fcd82ae2153e583387f3f33adc0
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-07-16 21:30:00 -0400

    now working on events, ready.js basic version should be complete

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,8 @@
-// placeholder for ready.js so new folders can be committed
+const config = require('../config.json');
+
+module.exports = async client => {
+    let single = client.guilds.size === 1;
+    console.log('Ready!');
+
+    client.user.setGame(`${config.prefix}help | ${client.guilds.size} server${single ? '' : 's'}`);
+};
