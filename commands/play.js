7b3572f7bb914ede3379288022ce0f8d3aedffde
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-07-21 22:23:09 -0400

    fixes typo in play.js (..config -> ../config)

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const embed = new Discord.RichEmbed();
 const embedCheck = require('../functions/embedPerms.js');
-const config = require('..config.json');
+const config = require('../config.json');
 const playNext = require('../functions/playNext.js');
 const YT_API = require('simple-youtube-api');
 const { parse } = require('url');
