d3420cb32e563ddac76438f08bf9817540934dcc
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-07-26 21:48:58 -0400

    changes setGame() to setActivity() as per Discord notes

--- events/ready.js
+++ events/ready.js
@@ -4,5 +4,5 @@ module.exports = async client => {
     let single = client.guilds.size === 1;
     console.log('Ready!');
 
-    client.user.setGame(`${config.prefix}help | ${client.guilds.size} server${single ? '' : 's'}`);
+    client.user.setActivity(`${config.prefix}help | ${client.guilds.size} server${single ? '' : 's'}`);
 };
