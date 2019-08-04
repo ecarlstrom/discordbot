e50f5b431f0a99bf0c2609a50a4bf11e4766bb44
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-08-03 21:08:34 -0400

    adds conditionals for severe weather alerts and sidebar color based on temperature

--- index.js
+++ index.js
@@ -339,6 +339,18 @@ client.on('message', (message) => {
           .setThumbnail(`${weatherOutput.imageUrl}`)
           .setTimestamp()
           
+          // changes color scheme (siedbar for now, maybe text later) based on temperature
+
+          if(temp >= 80) {
+            embed.setColor(0xFF6700)
+          } else if(temp >= 60 || temp <= 79) {
+            embed.setColor(0xADFF2F)
+          } else {
+            embed.setColor(0x00FFFF)
+          }
+
+          // displays severe weather alerts if any, otherwise gives an all-clear
+          
           if(!location.alert) {
             embed.addField(`No weather alerts!`, `👍`)
             embed.addBlankField(true)
