1514c254ba22916e31152c85ee35b16ae075b524
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-08-06 20:05:21 -0400

    added basic message/elevation/permission level work to index.js again, will be remaking commands as well

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const config = require('./config.json');
 const moment = require('moment');
 // will add another dependency for music bot event handling when the folder is up
 
+require('.util/eventLoader')(client);
+
 ////////// LEAGUE DEPENDENCIES //////////
 
 const fs = require('fs');
@@ -17,11 +19,6 @@ const gm = require('gm');
 const request = require('request');
 const urlencode = ('urlencode');
 const roundTo = require('round-to');
-
-// more to come as this is set up
-
-require('./util/eventLoader')(client);
-/////////////////////////////////////////
 
 ////////// WEATHER DEPENDENCIES //////////
 
@@ -40,6 +37,7 @@ client.on('ready', () => {
 // some general message rules
 
 const prefix = process.env.prefix;
+const musicPrefix = config.musicPrefix;
 const weatherPrefix = config.weatherPrefix;
 const forecastPrefix = config.forecastPrefix;
 
@@ -190,6 +188,87 @@ client.on('message', (message) => {
 
 /////////////////////////////// ***** MUSIC BOT ***** ///////////////////////////////
 
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.queues = new Discord.Collection();
+
+fs.readdir('./commands/', (err, files) => {
+  if(err) {
+    console.error(err);
+  }
+
+  log(`Loading a total of ${files.length} commands.`);
+  files.forEach(file => {
+    let props = require('./commands/${file}');
+
+    log(`Loading ${props.help.name} command. `);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./commands/${command}`)];
+
+      let cmd = require(`./commands/${command}`);
+
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if(cmd === command) {
+          client.aliases.delete(alias);
+        }
+      });
+
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch(e) {
+      reject(e);
+    }
+  });
+};
+
+client.elevation = message => {
+  let permlvl = 0;
+
+  if(message.author.id === config.owner) {
+    return permlvl = 10;
+  }
+
+  if(!message.guild) {
+    return permlvl;
+  }
+
+  if(message.guild) {
+    let mod_role = message.guild.roles.find('name', config.modRole);
+    if(mod_role && message.member.roles.has(mod_role.id)) permlvl = 2;
+
+    let super_mod_role = message.guild.roles.find('name', config.superModRole);
+    if(super_mod_role && message.member.roles.has(super_mod_role.id)) permlvl = 3;
+
+    let admin_role = message.guild.roles.find('name', config.adminRole); 
+    if(admin_role && message.member.roles.has(admin_role.id)) permlvl = 4;
+
+    if(message.author.id === message.guild.owner.id) {
+      permlvl = 5;
+    }
+    return permlvl;
+  };
+
+  process.on('unhandledRejection', err => {
+    console.error('Uncaught Promise Error: \n' + err);
+  });
+}
 
 /////////////////////////////// ***** WEATHER ***** ///////////////////////////////
 
