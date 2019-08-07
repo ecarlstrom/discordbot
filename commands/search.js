e563844aca79e844cb02cd53945a5e0af2a8ec80
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-08-07 03:32:09 -0400

    adds search command via search.js

---
+++ commands/search.js
@@ -0,0 +1,28 @@
+const config = require('../config.json');
+const ytapi = require('simple-youtube-api');
+const youtube = new ytapi(config.youtubeAPIKey);
+
+exports.run = async (client, message, args) => {
+    let search = args.join(' ');
+
+    try {
+        const results = await youtube.searchVideos(search, 5);
+        return message.channel.sendCode('', `Top 5 search results:\n\n🤠 ${results.map(i =>
+            `${i.title}\n https://www.youtube.com/watch?v=${i.id}\n`).join('\n 🤠 ')}`);
+        } catch(e) {
+            message.reply(e.message);
+        }
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+};
+
+exports.help = {
+    name: 'search',
+    description: 'Returns the top 5 YouTube search results for the given term.',
+    usage: 'search (term)'
+};
