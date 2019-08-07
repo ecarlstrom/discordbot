d3e6e3e5eac7ccaec5c113c534a51dd86710e397
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-08-07 02:54:29 -0400

    creates pause command via pause.js

---
+++ commands/pause.js
@@ -0,0 +1,27 @@
+exports.run = async (client, message) => {
+    const voiceChannel = message.member.voiceChannel ? message.member.voiceChannel : (message.guild.voiceConnection ? message.guild.voiceConnection.channel : null);
+
+    if(!voiceChannel || (!message.member.voiceChannel && message.author.permLevel < 2)) {
+        return message.reply(`🤠 Please join a voice channel! 🤠 `);
+    }
+
+    if(client.queues.get(message.guild.id).dispatcher.paused) {
+        return message.reply('Playback is already paused!');
+    }
+
+    await message.channel.sendMessage('Pausing playback!');
+    client.queues.get(message.guild.id).dispatcher.pause();
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['p'],
+    permLevel: 0
+};
+
+exports.help = {
+    name: 'pause',
+    description: 'Pauses the stream.',
+    usage: 'Pause'
+};
