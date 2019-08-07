066a6050f7e5f485fdfa0a93d14fd6fe3dd00114
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-08-07 04:48:25 -0400

    edits prefix variable name in ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,5 +4,5 @@ module.exports = async client => {
     let single = client.guilds.size === 1;
     console.log('Ready!');
 
-    client.user.setActivity(`${config.prefix}help | ${client.guilds.size} server${single ? '' : 's'}`);
+    client.user.setActivity(`${config.musicPrefix}help | ${client.guilds.size} server${single ? '' : 's'}`);
 };
