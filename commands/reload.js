4da7dc1c28b475884effe6ce518eb0985e2acabb
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-08-08 03:22:03 -0400

    creates reload command via reload.js

---
+++ commands/reload.js
@@ -0,0 +1,36 @@
+exports.run = async(client, msg, params) => {
+    let command;
+
+    if(client.commands.has(params[0])) {
+        command = params[0];
+    } else if(client.aliases.has(params[0])) {
+        command = client.aliases.get(params[0]);
+    }
+
+    if(!command) {
+        return msg.channel.sendMessage(`🤠 Sorry, cannot find ${params[0]} command! 🤠`);
+    }
+
+    const m = await msg.channel.sendMessage(`🤠 Reloading ${command} command! 🤠`);
+
+    try {
+        await client.reload(command);
+    } catch(e) {
+        return m.edit(`Failed command reload: ${command}\n\`\`\`${e.stack}\`\`\``);
+    }
+
+    return m.edit(`🤠 Command ${command} successfully reloaded! 🤠`);
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['rld'],
+    permLevel: 3
+};
+
+exports.help = {
+    name: 'reload',
+    description: 'Reloads the given command file in case of issues or updates.',
+    usage: 'reload/rld (commandname)'
+};
