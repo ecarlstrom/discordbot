107fdb65fba5047a46505eec201cb93911cc6cf9
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-08-09 15:38:49 -0400

    Procfile edits

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,8 @@
 const config = require('../config.json');
 
 module.exports = async client => {
-    let single = client.guilds.size === 1;
+    // let single = client.guilds.size === 1;
     console.log('Ready!');
 
-    client.user.setActivity(`${config.musicPrefix}help | ${client.guilds.size} server${single ? '' : 's'}`);
+    client.user.setActivity(`!help | ${config.musicPrefix}help`);
 };
