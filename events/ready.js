88b386e1bd9ececf8dcede78e89f1c128cf4e89a
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-08-09 16:59:30 -0400

    minor setGame adjustment

--- events/ready.js
+++ events/ready.js
@@ -4,5 +4,5 @@ module.exports = async client => {
     // let single = client.guilds.size === 1;
     console.log('Ready!');
 
-    client.user.setActivity(`!help | ${process.env.musicPrefix}help`);
+    client.user.setActivity(`!help | !mhelp`);
 };
