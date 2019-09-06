f9d840fb9a378a889992d5eb306ace17e027c446
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-09-06 16:01:25 -0400

    fixes message.js indentation

--- events/message.js
+++ events/message.js
@@ -3,39 +3,39 @@ const config = require('dotenv').config();
 module.exports = async message => {
     let client = message.client;
     try {
-    if(message.author.bot) {
-        return;
-    }
-
-    if(message.author.id === client.user.id) {
-        return;
-    }
-
-    if(!message.content.startsWith(process.env.prefix)) {
-        return;
-    }
-
-    let command = message.content.split(' ')[0].slice(process.env.prefix.length);
-    let params = message.content.split(' ').slice(1);
-    let perms = client.elevation(message);
-
-    message.author.permLevel = perms;
-
-    let cmd;
-    if(client.commands.has(command)) {
-        cmd = client.commands.get(command);
-    } else if(client.aliases.has(command)) {
-        cmd = client.commands.get(client.aliases.get(command));
-    }
-
-    if(cmd) {
-        if(perms < cmd.conf.permLevel) {
+        if(message.author.bot) {
             return;
         }
 
-        cmd.run(client, message, params, perms);
-    }
-} catch(err) {
+        if(message.author.id === client.user.id) {
+            return;
+        }
+
+        if(!message.content.startsWith(process.env.prefix)) {
+            return;
+        }
+
+        let command = message.content.split(' ')[0].slice(process.env.prefix.length);
+        let params = message.content.split(' ').slice(1);
+        let perms = client.elevation(message);
+
+        message.author.permLevel = perms;
+
+        let cmd;
+        if(client.commands.has(command)) {
+            cmd = client.commands.get(command);
+        } else if(client.aliases.has(command)) {
+            cmd = client.commands.get(client.aliases.get(command));
+        }
+
+        if(cmd) {
+            if(perms < cmd.conf.permLevel) {
+                return;
+            }
+
+            cmd.run(client, message, params, perms);
+        }
+        } catch(err) {
     return;
-}
+    }
 };
