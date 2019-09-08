ea40637f5256dcd275059a6c7f10fa327697fa95
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-09-07 20:08:30 -0400

    tests simplified play to investigate hosting issues

--- commands/play.js
+++ commands/play.js
@@ -1,101 +1,26 @@
-const Discord = require('discord.js');
-const embed = new Discord.RichEmbed();
-const embedCheck = require('../functions/embedPerms.js');
-const config = require('dotenv').config();
-const playNext = require('../functions/playNext.js');
-const ytapi = require('simple-youtube-api');
-const { parse } = require('url');
-const youtube = new ytapi(process.env.youtubeAPIKey);
+const ytdl = require('ytdl-core');
 
 exports.run = async (client, message, args) => {
-    let song = args.join(' ');
-
-    if(!song.length) {
-        return message.reply(`🤠 Please use a YouTube URL or search term! 🤠`);
+    if(!message.member.voiceChannel) {
+        return message.channel.send(`🤠 Please enter a voice channel! 🤠`);
     }
 
-    const voiceChannel = message.member.voiceChannel ? message.member.voiceChannel : (message.guild.voiceConnection ? message.guild.voiceConnection.channel : null);
-
-    if(!voiceChannel || (!message.member.voiceChannel && message.author.permLevel < 2)) {
-        return message.reply(`🤠 Please join a voice channel! 🤠`);
+    if(!args[0]) {
+        return message.channel.send(`🤠 Please input a search term or URL! 🤠`);
     }
 
-    if(!client.queues.has(message.guild.id)) {
-        var firstSong = true;
-        client.queues.set(message.guild.id, {
-            dispatcher: null,
-            queue: [],
-            connection: null,
-            position: -1
-        });
-        await voiceChannel.join();
+    let validate = await ytdl.validateURL(args[0]);
+
+    if(!validate) {
+        return message.channel.send(`🤠 Please use a valid term! 🤠`);
     }
 
-    let id = (() => {
-        const parsed = parse(song, true);
+    let info = await ytdl.getInfo(args[0]);
+    let connection = await message.member.voiceChannel.join();
+    let dispatcher = await connection.playStream(ytdl(args[0], { filter: 'audioonly' }));
 
-        if(/^(www\.)?youtube\.com/.test(parsed.hostname)) {
-            return parsed.query.v;
-        } else if(/^(www\.)?youtu\.be/.test(parsed.hostname)) {
-            return parsed.pathname.slice(1);
-        }
-    })();
-
-    if(!id) {
-        let results = await youtube.searchVideos(song, 4);
-        id = results[0].id;
-    }
-
-    let info;
-    try {
-        // console.log(id);
-        info = await youtube.getVideoByID(id);
-        // console.log('again: ' + id);
-    } catch(e) {
-        console.log(e.stack || e);
-    }
-
-    if(message.author.permLevel < 2 && parseInt(info.durationSeconds) > 900) {
-        return message.reply(`🤠 Sorry, song time limit is 15 minutes! 🤠`).catch(console.error.message);
-    }
-
-    let time = parseInt(info.durationSeconds, 10);
-    let minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-    if(seconds < 10) {
-        seconds = '0' + seconds;
-    }
-
-    client.queues.get(message.guild.id).queue.push({
-        url: `https://www.youtube.com/watch?v=${info.id}`,
-        id: info.id,
-        channName: info.channel.title,
-        songTitle: info.title,
-        playTime: `${minutes}:${seconds}`,
-        playTimeSeconds: info.durationSeconds,
-        requester: message.guild.member(message.author).displayName,
-        requesterIcon: message.author.avatarURL
-    });
-
-    if(firstSong) {
-        playNext(message);
-    } else {
-        embed
-            .setTitle(`**${info.title}** (${minutes}:${seconds}) added to queue!`)
-            .setColor(0xc10404)
-            .setFooter(`Requested by ${message.guild.member(message.author).displayName}`, message.author.avatarURL)
-            .setImage(`https://i.ytimg.com/vi/${info.id}/mqdefault.jpg`)
-            .setTimestamp()
-            .setURL(`https://www.youtube.com/watch?v=${info.id}`);
-        if(embedCheck(message)) {
-            message.channel.send(embed, '', {
-                disableEveryone: true
-            }).catch(console.error);
-        } else {
-            message.channel.send(`**${info.title}** (${minutes}:${seconds}) added to queue!`);
-        }
-    }
-};
+    message.channel.send(`🤠 Now playing: ${info.title} 🤠`);
+}
 
 exports.conf = {
     enabled: true,
