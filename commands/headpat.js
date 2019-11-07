f6edc2554df6c6a656908b265b470afc477a3944
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-11-06 22:19:00 -0500

    adds function idea for parsing mention

--- commands/headpat.js
+++ commands/headpat.js
@@ -8,7 +8,23 @@ exports.run = (client, message) => {
     };
 
     // can add something for mentioning other users, maybe in a new command?
-    
+    // parse the username with a function, e.g.:
+
+    // function retrieveUser(mention) {
+    //     if(!mention) {
+    //         return;
+    //     }
+
+    //     if(mention.startsWith('@') && mention.endsWith('>')) {
+    //         mention = mention.slice(2, -1); // to get the ID
+
+    //         if(mention.startsWith('!')) {
+    //             mention = mention.slice(1);
+    //         }
+
+    //         return client.users.get(mention);
+    //     }
+    // }
 }
 
 exports.conf = {
