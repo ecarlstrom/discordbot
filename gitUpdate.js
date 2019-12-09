06c042066d4270e6cdc907a27cbedef460ade7e8
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-12-08 22:11:50 -0500

    adds dependencies and variables for gitUpdate.js

---
+++ gitUpdate.js
@@ -0,0 +1,17 @@
+// should keep the bot updated with the git repo
+// will add into commands folder when work is done so it doesn't cause an issue with current command loading on boot
+
+// need in config: Git username, password, repo URL
+
+const {
+    exec
+} = require('child_process');
+const path = require('path');
+const {
+    URL
+} = require('url');
+
+const {
+    run: reboot
+} = require('./reboot');
+// possibly separate config for this file
