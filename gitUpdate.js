b0a5d0ebd7edf26b60d8c4a7faf4f96760127808
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-12-08 22:15:14 -0500

    sets up basic gitUpdate idea

--- gitUpdate.js
+++ gitUpdate.js
@@ -3,15 +3,19 @@
 
 // need in config: Git username, password, repo URL
 
-const {
-    exec
-} = require('child_process');
-const path = require('path');
-const {
-    URL
-} = require('url');
+// const {
+//     exec
+// } = require('child_process');
+// const path = require('path');
+// const {
+//     URL
+// } = require('url');
 
-const {
-    run: reboot
-} = require('./reboot');
+// const {
+//     run: reboot
+// } = require('./reboot');
 // possibly separate config for this file
+
+// exports.run = async(client, message, args) => {
+//     // add git config info
+// }
