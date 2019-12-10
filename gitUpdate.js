b13ac9ce4b4e70cf9d5ea28380169b778f713283
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-12-09 22:21:20 -0500

    continues gitUpdate work

--- gitUpdate.js
+++ gitUpdate.js
@@ -1,3 +1,6 @@
+import { exec } from "child_process";
+import { pathToFileURL } from "url";
+
 // should keep the bot updated with the git repo
 // will add into commands folder when work is done so it doesn't cause an issue with current command loading on boot
 
@@ -18,4 +21,21 @@
 
 // exports.run = async(client, message, args) => {
 //     // add git config info
+
+        exec(`git pull ${giturlhere}`, {
+            cwd: pathToFileURL.join(__dirname, '../')
+        }, async(err, stdout, stderr) => {
+            if(err) {
+                return console.error(err);
+            }
+
+            const out = [];
+            if(stdout) {
+                out.push(stdout);
+            }
+            if(stderr) {
+                out.push(stderr);
+            }
+        }
+        }
 // }
