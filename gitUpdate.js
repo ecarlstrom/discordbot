743644f54e6840927c3f372f60c022923d77b175
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-12-10 22:27:59 -0500

    adds variables to gitUpdate

--- gitUpdate.js
+++ gitUpdate.js
@@ -1,6 +1,8 @@
 import { exec } from "child_process";
 import { pathToFileURL } from "url";
 
+const config = require('dotenv').config();
+
 // should keep the bot updated with the git repo
 // will add into commands folder when work is done so it doesn't cause an issue with current command loading on boot
 
@@ -20,25 +22,28 @@ import { pathToFileURL } from "url";
 // } = require('./reboot');
 // possibly separate config for this file
 
-// exports.run = async(client, message, args) => {
-//     // add git config info
+exports.run = async(client, message, args) => {
+    // add git config info
+    const gitURL = new URL(config.gitURL);
+    gitURL.password = config.gitPass;
+    gitURL.username = config.gitHandle;
 
-//         exec(`git pull ${giturlhere}`, {
-//             cwd: pathToFileURL.join(__dirname, '../')
-//         }, async(err, stdout, stderr) => {
-//             if(err) {
-//                 return console.error(err);
-//             }
+        exec(`git pull ${gitURL}`, {
+            cwd: pathToFileURL.join(__dirname, '../')
+        }, async(err, stdout, stderr) => {
+            if(err) {
+                return console.error(err);
+            }
 
-//             const out = [];
-//             if(stdout) {
-//                 out.push(stdout);
-//             }
-//             if(stderr) {
-//                 out.push(stderr);
-//             }
+            const out = [];
+            if(stdout) {
+                out.push(stdout);
+            }
+            if(stderr) {
+                out.push(stderr);
+            }
 
-//             await message.channel.sendMessage(`\`\`\`${out.join('```\n```')}\`\`\``);
-//             return reboot(client, message, args);
-//         });
-// };
+            await message.channel.sendMessage(`\`\`\`${out.join('```\n```')}\`\`\``);
+            return reboot(client, message, args);
+        });
+};
