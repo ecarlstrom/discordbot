a5954efb4d835ab94813e843e94629c3c3426374
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-12-12 22:14:45 -0500

    undoes previous error that took out all comments

--- gitUpdate.js
+++ gitUpdate.js
@@ -3,11 +3,11 @@ import { pathToFileURL } from "url";
 
 const config = require('dotenv').config();
 
-should keep the bot updated with the git repo
-will add into commands folder when work is done so it doesn't cause an issue with current command loading on boot
+// should keep the bot updated with the git repo
+// will add into commands folder when work is done so it doesn't cause an issue with current command loading on boot
 
-need in config: Git username, password, repo URL
-git info now added to config, testing variables
+// need in config: Git username, password, repo URL
+// git info now added to config, testing variables
 
 const {
     exec
@@ -20,7 +20,7 @@ const {
 const {
     run: reboot
 } = require('./reboot');
-possibly separate config for this file
+// possibly separate config for this file
 
 exports.run = async(client, message, args) => {
     // add git config info
