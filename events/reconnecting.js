b966de3fd6d712b1da97927b458da03cec5ae9b1
Evan Carlstrom <evan.carlstrom@gmail.com>
2020-04-10 23:31:56 -0400

    updates reconnecting.js

--- events/reconnecting.js
+++ events/reconnecting.js
@@ -1,6 +1,7 @@
 module.exports = async client => {
     // test auto-reconnect on process kill?
     // 4/5: bot is functioning normally but reconnecting once or twice every minute, investigating this.
+    // 4/10: maybe the reconnects are host-related, everything on this end seems fine
     try {
         console.log('Reconnecting!');
     }catch(err) {
