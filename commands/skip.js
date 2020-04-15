4fba57d4b8caa4a643262a9fe93772fcf94c4723
Evan Carlstrom <evan.carlstrom@gmail.com>
2020-04-14 20:54:12 -0400

    updates skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,5 +1,4 @@
 exports.run = async (client, message) => {
-    // 3/31: possibly more skip errors in certain scenarios
     const voiceChannel = message.member.voiceChannel ? message.member.voiceChannel : (message.guild.voiceConnection ? message.guild.voiceConnection.channel : null); 
 
     if(!voiceChannel || (!message.member.voiceChannel)) {
